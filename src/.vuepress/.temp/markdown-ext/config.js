c9887c1ac698b94d1b5be8af550c7c6b12d0f4e4
lizhuo <2636050454@qq.com>
2025-04-28 17:29:01 +0800

    first-commit

---
+++ src/.vuepress/.temp/markdown-ext/config.js
@@ -0,0 +1,2 @@
+    import "E:/blog/my-docs/node_modules/@vuepress/plugin-markdown-ext/lib/client/styles/footnote.css"
+    import "E:/blog/my-docs/node_modules/@vuepress/plugin-markdown-ext/lib/client/styles/tasklist.css"
---
+++ src/.vuepress/.temp/markdown-stylize/config.js
@@ -0,0 +1,3 @@
+import "E:/blog/my-docs/node_modules/@mdit/plugin-spoiler/spoiler.css"
+
+
